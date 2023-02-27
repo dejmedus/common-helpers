1bcb9ea6e161a1f33adb45d613055f809daef13c
dejmedus <email96123@gmail.com>
2023-02-27 16:21:01 -0700

    add logic to get dependencies from requirements.txt files

--- tools/generate-readme.js
+++ tools/generate-readme.js
@@ -3,90 +3,96 @@
 const { writeFile, readFile } = require('node:fs/promises');
 const { resolve } = require('node:path');
 
+const fs = require('fs')
+
+
+
 async function generate(repoName) {
     let filePath;
-    try {
-        if (resolve('./package.json') !== null) {
-            filePath = resolve('./package.json');
-        }
-        else if (resolve('./requirements.txt')) {
-            filePath = resolve('./requirements.txt');
-        }
-    }
-    catch (err) {
-        console.log("Cant find a dependencies file");
-    }
-
     let readmeDeps = [];
     let runCommand = '';
 
-    if (filePath !== null) {
+    try {
+
+        if (fs.existsSync('./package.json')) {
+            filePath = resolve('./package.json');
+        }
+        else if (fs.existsSync('./requirements.txt')) {
+            filePath = resolve('./requirements.txt')
+        }
+    }
+    catch (err) {
+        console.log("Error while searching for a dependency file", err);
+    }
+
+    if (filePath !== undefined) {
+        let dependencies = [];
         try {
             const contents = await readFile(filePath, { encoding: 'utf8' });
-            const dependencyKeyList = Object.keys(dependencyKey);
+
 
             if (filePath.endsWith('.json')) {
                 const packageFile = JSON.parse(contents);
-                let dependencies = [...Object.keys(packageFile.dependencies), ...Object.keys(packageFile.devDependencies)];
+                dependencies = [...Object.keys(packageFile.dependencies), ...Object.keys(packageFile.devDependencies)];
 
                 // clean up dependency list
                 for (let i = 0; i < dependencies.length; i++) {
-                    dependencies[i] = dependencies[i].split("/")
-                    dependencies[i] = dependencies[i][0].replace("@", "");
+                    dependencies[i] = dependencies[i].split("/")[0].replace("@", "")
                 }
-                dependencies = [...new Set(dependencies)];
-
-                //
-                for (let i = 0; i < dependencies.length; i++) {
-                    if (dependencyKeyList.includes(dependencies[i])) {
-                        readmeDeps.push(`- [${dependencyKey[dependencies[i]].name}](${dependencyKey[dependencies[i]].link}) ${dependencyKey[dependencies[i]].desc}`);
-                    }
-
-                    if (hasRunCommands.includes(dependencies[i])) {
-                        runCommand = dependencyKey[dependencies[i]].command;
-                    }
-                }
-                console.log(readmeDeps);
             }
             else if (filePath.endsWith('.txt')) {
-                // requirements.txt
+                contents.split('\n').forEach((dep, i) => {
+                    dependencies[i] = dep.split("==")[0];
+                });
             }
 
         } catch (err) {
-            console.error('Could not access dependency file', err);
-            process.exit(1);
+            console.error('Could not access dependency file');
+        }
+
+        const dependencyKeyList = Object.keys(dependencyKey);
+
+        dependencies = [...new Set(dependencies)].filter(Boolean);
+
+
+        // create readmeDeps
+        for (let i = 0; i < dependencies.length; i++) {
+            if (dependencyKeyList.includes(dependencies[i])) {
+                readmeDeps.push(`- [${dependencyKey[dependencies[i]].name}](${dependencyKey[dependencies[i]].link}) ${dependencyKey[dependencies[i]].desc}`);
+            }
+
+            if (hasRunCommands.includes(dependencies[i])) {
+                runCommand = dependencyKey[dependencies[i]].command;
+            }
         }
     }
 
 
-    const filename = repoName.toUpperCase().trim().replace(" ", '-');
+    const filename = repoName.toLowerCase().trim().replace(" ", '-');
+
 
     const commandString = runCommand !== "" ? [
+        `\n4. Run the app\n`,
         '```shell',
         `${runCommand}`,
         '```',
     ].join('\n') : "";
 
-    const depString = readmeDeps.length > 0 ? readmeDeps.join('\n') : "";
+    const depString = readmeDeps.length > 0 ? readmeDeps.shift('## Built with\n').join('\n') : "";
 
 
     try {
         await writeFile(`./${filename}-README.md`, [
             `# ${repoName}`,
-            '\n',
-            '## Built with\n',
             `${depString}`,
-            '\n',
             '## Local Development Setup\n',
-            `1. [Fork](https://docs.github.com/en/get-started/quickstart/fork-a-repo) ${repoName}.`,
-            `2. [Create a local clone](https://docs.github.com/en/get-started/quickstart/fork-a-repo#cloning-your-forked-repository) of the forked repository.`,
+            `1. [Fork](https://docs.github.com/en/get-started/quickstart/fork-a-repo) ${filename}`,
+            `2. [Create a local clone](https://docs.github.com/en/get-started/quickstart/fork-a-repo#cloning-your-forked-repository) of the forked repository`,
             `3. Using the terminal, move into your local copy\n`,
             '```shell',
-            `cd ${repoName}`,
-            '```\n',
-            `4. Run the app\n`,
+            `cd ${filename}`,
+            '```',
             `${commandString}`,
-            '\n',
             '## Contributing',
             '1. Create a new branch',
             '```shell',
@@ -114,24 +120,8 @@ async function generate(repoName) {
 
 module.exports.generate = generate;
 
-const hasRunCommands = ['react', 'next', 'flask'];
+const hasRunCommands = ['react', 'next', 'Flask'];
 const dependencyKey = {
-    flask: {
-        name: 'Flask',
-        link: 'https://flask.palletsprojects.com/en/',
-        desc: 'a python web framework',
-        command: 'flask --debug run'
-    },
-    jinja: {
-        name: 'Jinja',
-        link: 'https://palletsprojects.com/p/jinja/',
-        desc: 'template engine'
-    },
-    sqlite: {
-        name: 'SQLite',
-        link: 'https://www.sqlite.org/index.html',
-        desc: 'SQL database'
-    },
     python3: {
         name: 'Python 3',
         link: 'https://www.python.org/downloads/',
@@ -154,9 +144,30 @@ const dependencyKey = {
         desc: 'full-stack React framework',
         command: 'npm run dev',
     },
+    Flask: {
+        name: 'Flask',
+        link: 'https://flask.palletsprojects.com/en/',
+        desc: 'a python web framework',
+        command: 'flask --debug run'
+    },
+    Jinja2: {
+        name: 'Jinja',
+        link: 'https://palletsprojects.com/p/jinja/',
+        desc: 'template engine'
+    },
     docusaurus: {
         name: 'Docusaurus',
         link: 'https://docusaurus.io/',
         desc: 'React static-site generator',
     },
+    sqlite: {
+        name: 'SQLite',
+        link: 'https://www.sqlite.org/index.html',
+        desc: 'SQL database'
+    },
+    numpy: {
+        name: 'NumPy',
+        link: 'https://numpy.org/',
+        desc: 'numerical computing package'
+    }
 }
